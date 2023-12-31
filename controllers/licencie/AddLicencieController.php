45da0b44968486679234bdadebfb13460f1c88be
Emmanuel <yeoepro921@gmail.com>
2023-12-31 10:10:13 +0100

    Ajouter licencié ✔️

--- controllers/licencie/AddLicencieController.php
+++ controllers/licencie/AddLicencieController.php
@@ -3,25 +3,31 @@
 class AddLicencieController {
     
     private LicencieDAO $licenceDAO;
-    public function __construct(LicencieDAO $licenceDAO) {
+    private CategorieDAO $categorieDAO;
+
+
+    public function __construct(LicencieDAO $licenceDAO, CategorieDAO $categorieDAO) {
         $this->licenceDAO = $licenceDAO;
+        $this->categorieDAO = $categorieDAO;
     }
 
     public function index() {
         // Include la vue pour afficher le formulaire
+        $categories = $this->categorieDAO->getAll();
         include('../../views/licencie/add_licencie.php');
     }
 
     public function addLicencie() {
         if ($_SERVER['REQUEST_METHOD'] === 'POST') {
             // Récupérer les données du controlleur
+            $numLicencie = $_POST['numLicence'];
             $nomLicencie = $_POST['nomLicencie'];
             $prenomLicencie = $_POST['prenomLicencie'];
             $categorieId = $_POST['categorieId'];
 
             // Valider les données du formulaire
 
-            $nouveauLicencie = new LicencieModel(0, 0, $nomLicencie, $prenomLicencie, $categorieId);
+            $nouveauLicencie = new LicencieModel(0, $numLicencie, $nomLicencie, $prenomLicencie, $categorieId);
 
             if ($this->licenceDAO->create($nouveauLicencie)) {
                 header('Location: HomeLicencieController.php');
@@ -36,10 +42,14 @@ class AddLicencieController {
 
 require_once("../../config/config.php");
 require_once("../../classes/models/LicencieModel.php");
+require_once("../../classes/models/CategorieModel.php");
+
 require_once("../../classes/dao/LicencieDAO.php");
+require_once("../../classes/dao/CategorieDAO.php");
 global $pdo;
 $licencieDAO = new LicencieDAO($pdo);
-$controller = new AddLicencieController($licencieDAO);
+$categorieDAO = new CategorieDAO($pdo);
+$controller = new AddLicencieController($licencieDAO, $categorieDAO);
 if (!isset($_POST['action'])) {
     $controller->index();
 } else {
--- views/licencie/add_licencie.php
+++ views/licencie/add_licencie.php
@@ -2,12 +2,12 @@
 <html lang="fr">
 <head>
     <meta charset="UTF-8">
-    <title>Ajouter un licencie</title>
+    <title>Ajouter un licencié</title>
     <!-- Ajoutez ici vos liens CSS ou styles pour la mise en forme -->
 </head>
 <body>
-    <h1>Ajouter une licencie </h1>
-    <a href="../../views/licencie/home.php">Retour a la liste des licenciés</a>
+    <h1>Ajouter un licencié </h1>
+    <a href="../../views/licencie/home.php">Retour à la liste des licenciés</a>
 
     <form action="../../controllers/licencie/AddLicencieController.php" method="post">
         <label for="nomLicencie">Nom:</label>
@@ -17,7 +17,18 @@
         <input type="text" id="prenomLicencie" name="prenomLicencie" required><br>
 
         <!--Categorie-->
+        <label for="categorieId">Catégorie</label>
+        <select name="categorieId" id="categorieId">
+            <?php foreach ($categories as $categorie): ?>
+                <option value="<?= $categorie->getIdCategorie(); ?>">
+                    <?= $categorie->getNomCategorie(); ?>
+                </option>
+            <?php endforeach; ?>     
+        
+        </select><br>
 
+        <label for="numLicence">Numéro de licence :</label>
+        <input type="text" id="numLicence" name="numLicence" required><br>
 
         <input type="submit" name="action" value="Ajouter">
     </form>
